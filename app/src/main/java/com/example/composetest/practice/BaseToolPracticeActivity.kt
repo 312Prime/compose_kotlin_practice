d9b4d0ca5b2b82806d09864301ca64aa3aa28483
sangil <fhemflrh60@gmail.com>
2023-11-24 00:06:38 +0900

    feat: Box 기능 테스트

--- app/src/main/java/com/example/composetest/practice/BaseToolPracticeActivity.kt
+++ app/src/main/java/com/example/composetest/practice/BaseToolPracticeActivity.kt
@@ -8,13 +8,16 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -49,7 +52,7 @@ class BaseToolPracticeActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTestTheme {
-                WebImageTest()
+                BoxExample()
             }
         }
     }
@@ -168,7 +171,7 @@ fun MyTextField() {
 }
 
 @Composable
-fun MyImageTest(){
+fun MyImageTest() {
     Image(
         painter = painterResource(id = R.drawable.ic_launcher_background),
         contentDescription = "icon"
@@ -176,17 +179,102 @@ fun MyImageTest(){
 }
 
 @Composable
-fun WebImageTest(){
+fun WebImageTest() {
     AsyncImage(
         model = "https://s3.ap-northeast-2.amazonaws.com/img.kormedi.com/news/article/__icsFiles/artimage/2016/12/12/c_km601/403416_540_1.jpg",
         contentDescription = "cheese"
     )
 }
 
+@Composable
+fun BoxExample() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Red)
+    ) {
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Blue)
+                .padding(16.dp)
+                .align(Alignment.TopStart)
+        ) {
+            Text(text = "왼쪽 위")
+        }
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Green)
+                .padding(16.dp)
+                .align(Alignment.TopCenter)
+        ) {
+            Text(text = "중앙 위")
+        }
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Gray)
+                .padding(16.dp)
+                .align(Alignment.TopEnd)
+        ) {
+            Text(text = "오른쪽 위")
+        }
+
+        Button(
+            onClick = {},
+            modifier = Modifier.align(Alignment.CenterStart)
+        ) {
+            Text(text = "중앙 왼쪽")
+        }
+        Button(
+            onClick = {},
+            modifier = Modifier.align(Alignment.Center)
+        ) {
+            Text(text = "중앙 중앙")
+        }
+        Button(
+            onClick = {},
+            modifier = Modifier.align(Alignment.CenterEnd)
+        ) {
+            Text(text = "중앙 오른쪽")
+        }
+
+
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Blue)
+                .padding(16.dp)
+                .align(Alignment.BottomStart)
+        ) {
+            Text(text = "왼쪽 아래")
+        }
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Green)
+                .padding(16.dp)
+                .align(Alignment.BottomCenter)
+        ) {
+            Text(text = "중앙 아래")
+        }
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color.Gray)
+                .padding(16.dp)
+                .align(Alignment.BottomEnd)
+        ) {
+            Text(text = "오른쪽 아래")
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeTestTheme {
-        MyImageTest()
+        BoxExample()
     }
 }
